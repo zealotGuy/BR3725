b54869fc3a2546845a253394aaaa8686630a11d7
Aryaman Singh <148281236+zealotGuy@users.noreply.github.com>
2025-03-07 13:41:18 -0800

    Update bitshift.py

--- bitshift.py
+++ bitshift.py
@@ -3,3 +3,9 @@ def multiply(n):
     #multiply a number by 2 using bitshift
     return n
 
+#Step 1: make a new variable g
+#Step 2: Create a loop which will loop through the binary
+#Step 3: for every time it goes through the binary, for a 1 it will carry out the equation 2^n and will add n+=1 as well as g
+#step 4: For every time it is 0 only g gets added by one
+#step 5: if there is a 1, set g = n and carry out the equation
+#Step 4: Add all of the numbers at the end
